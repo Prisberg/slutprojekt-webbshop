6c31fa1391d67dd37e3c0918ce0e95719284396e
jv98 <jacob@vahlen.se>
2022-03-20 15:34:20 +0100

    la till en till fil för wiew för info prudekter

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,10 +1,44 @@
-import { Box } from "@mui/material";
 
+import { 
+ Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow, } from "@mui/material";
+
+function createData(name: string, value: any) {
+  return { name, value };
+}
+
+const rows = [
+  createData('Name', 'Rolex'),
+  createData('Circumference', '46 mm'),
+  createData('Use', 'Have them around your wrist'),
+  createData('Back case', 'solid'),
+  createData('Glass', 'Diamond glass on top'),
+  createData('Thickness', '8 mm'),
+  createData('Bracelet', 'steel'),
+];
 function ProductInfo() {
     return (
-      <Box sx={{marginTop: '10rem'}}>
-          This is the product info page
-      </Box>
+      <TableContainer sx={{ 
+        justifyContent: 'center',
+        marginTop: '15rem',
+        display: 'flex',
+      }} >
+      <Table sx={{maxWidth: '30rem', textAlign: 'center',}}>
+          <TableBody>
+              {rows.map((row) => (
+                  <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                          {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.value}</TableCell>
+                  </TableRow>
+              ))}
+          </TableBody>
+      </Table>
+  </TableContainer>
     );
   }
   
