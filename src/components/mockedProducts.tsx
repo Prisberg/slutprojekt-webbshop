c30087e53c88f5ce2d1bebe4a8830326a9f48bb5
Erikisak <erik.isak@icloud.com>
2022-03-25 16:43:12 +0100

    new watches added

--- src/components/mockedProducts.tsx
+++ src/components/mockedProducts.tsx
@@ -1,6 +1,9 @@
 import Submariner from "../assets/images/SubmarinerGreen.jpg";
 import SubmarinerBlue from "../assets/images/Rolex-submariner.jpeg";
 import AirKing from "../assets/images/air-king.jpg";
+import Explorer from "../assets/images/Explorer.jpg";
+import Datejust from "../assets/images/Datejust.jpg";
+import Daydate from "../assets/images/daydate.jpg";
 
 export interface ProductInfo {
     id: number;
@@ -15,8 +18,8 @@ export const products: ProductInfo[] = [
     { id: 0, name: 'Rolex', model: 'Submariner' ,describtion: 'Green', price: 100000, image: Submariner }, 
     { id: 1, name: 'Rolex', model: 'Submariner' ,describtion: 'Blue', price: 150000, image: SubmarinerBlue },
     { id: 2, name: 'Rolex', model: 'Air-King' ,describtion: 'Silver', price: 120000, image: AirKing },
-    { id: 3, name: 'Rolex', model: 'Submariner' ,describtion: 'Green', price: 100000, image: Submariner }, 
-    { id: 4, name: 'Rolex', model: 'Submariner' ,describtion: 'Blue', price: 150000, image: SubmarinerBlue },
-    { id: 5, name: 'Rolex', model: 'Air-King' ,describtion: 'Silver', price: 120000, image: AirKing },
+    { id: 3, name: 'Rolex', model: 'Explorer' ,describtion: 'Black', price: 170000, image: Explorer }, 
+    { id: 4, name: 'Rolex', model: 'Datejust' ,describtion: 'Green', price: 160000, image: Datejust },
+    { id: 5, name: 'Rolex', model: 'Day-Date' ,describtion: 'Gold', price: 175000, image: Daydate },
 ]
 
