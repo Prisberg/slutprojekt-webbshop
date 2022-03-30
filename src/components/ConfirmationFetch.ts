53d557945b7e1eed59e32024c75a7bf0be889011
Erikisak <erik.isak@icloud.com>
2022-03-30 22:12:32 +0200

    Api update

---
+++ src/components/ConfirmationFetch.ts
@@ -0,0 +1,13 @@
+export async function fakeFetch(_: string){
+
+    return new Promise((resolve)=>{
+
+        setTimeout(()=>{
+
+            resolve({validate: "valid"});
+
+        }, 2000);
+
+    });
+
+}
